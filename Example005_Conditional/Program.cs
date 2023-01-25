dd45c5e9816192df90cf8ffcc6039da0abf2a4ce
Tikhon <loki_deleoni@live.com>
2023-01-26 00:41:26 +0300

    add Example005_Conditional

---
+++ Example005_Conditional/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "оля")
+{
+    Console.WriteLine("Ура! Наша Оля приехала!");
+} 
+else
+if(username.ToLower() == "ваня")
+{
+    Console.WriteLine("Ваня, не балуйся!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
